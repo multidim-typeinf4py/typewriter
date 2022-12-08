eb819c824c60b7762a0f51cbcd0c3baac1b0ea40
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-12-08 12:55:16 +0100

    Add cloning

---
+++ data/paper-dataset/README.txt
@@ -0,0 +1,9 @@
+This dataset contains the experimental setup and results of the evaluation of TypeWriter on the open-source data set:
+
+ * github_urls.txt: List of all projects in the dataset.
+
+ * open_source_training_files.txt: Files used for training the model.
+
+ * open_source_validation_files.txt: Files used for validating the model.
+
+ * open_source_results_argument.json and open_source_results_return.json: Type predictions by the TypeWriter model. For each type slot, we list the developer-provided annotation and the top-5 predictions by the model.
---
+++ data/paper-dataset/cloner.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+while IFS="" read -r repo; do
+    url="$(echo "$repo" | cut -d: -f-1,2)"
+    sha="$(echo "$repo" | cut -d: -f4)"
+    name="$(echo "$url" | cut -d "/" -f 5 | cut -d "." -f 1)"
+
+    if [[ ! -d "Repos/$name" ]]; then
+        mkdir -p "Repos/$name"
+        (cd "Repos/$name" &&
+            git init &&
+            git remote add origin "$url" &&
+            git fetch --depth 1 origin "$sha" &&
+            git checkout FETCH_HEAD)
+    else
+        echo "$name was already cloned, continuing"
+    fi
+done <github_urls.txt
