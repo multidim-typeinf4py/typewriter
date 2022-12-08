7f2d00e222fc80ac32430586eaec1ee87e7e8353
Benjamin Sparks <benjamin.sparks@protonmail.com>
2022-12-08 13:15:42 +0100

    skip asking

--- data/paper-dataset/cloner.sh
+++ data/paper-dataset/cloner.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 while IFS="" read -r repo; do
-    url="$(echo "$repo" | cut -d: -f-1,2)"
-    sha="$(echo "$repo" | cut -d: -f4)"
-    name="$(echo "$url" | cut -d "/" -f 5 | cut -d "." -f 1)"
+    url="$(echo $repo | cut -d: -f-1,2)"
+    sha="$(echo $repo | cut -d: -f4)"
+    name="$(echo $url | cut -d "/" -f 5 | cut -d "." -f 1)"
 
     if [[ ! -d "Repos/$name" ]]; then
         mkdir -p "Repos/$name"
         (cd "Repos/$name" &&
             git init &&
             git remote add origin "$url" &&
-            git fetch --depth 1 origin "$sha" &&
+            GIT_TERMINAL_PROMPT=0 git fetch --depth 1 origin "$sha" &&
             git checkout FETCH_HEAD)
     else
         echo "$name was already cloned, continuing"
